a2160b6165ca16162c3ad1fabff56d7130f69b9f
samiranbisal <samiranbisal775@gmail.com>
2024-09-18 20:49:35 +0530

    pg add

--- fontPage/models.py
+++ fontPage/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from tinymce.models import HTMLField
+from autoslug import AutoSlugField
 
 class fontpage(models.Model):
     name = models.TextField(max_length=50)
     title = models.CharField(max_length=100)
     description = HTMLField()
     # image = models.ImageField(upload_to='images/')
+    
+    new_slug = AutoSlugField(populate_from='name', unique=True, null=True, default=None)
 # Create your models here.
